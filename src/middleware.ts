cf6b279f9cfb44289d6ac2045d8cf6758a9b5904
mauricio gallego <mauricio.gallego@yum.com>
2025-04-26 18:00:19 -0500

    removing console log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import i18nConfig from "../i18nConfig";
 
 export async function middleware(request: NextRequest) {
-  console.log(request.nextUrl.pathname);
-  if (request.nextUrl.pathname !== "/save-the-date") {
+  if (!request.nextUrl.pathname.includes("save-the-date")) {
     return NextResponse.redirect(new URL("/save-the-date", request.url));
   }
   return i18nRouter(request, i18nConfig);
